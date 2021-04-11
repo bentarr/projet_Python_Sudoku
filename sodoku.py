9c5b79980b3dc02d49ae2442250c675cacdd8dac
Ziirkov <55087514+Ziirkov@users.noreply.github.com>
2021-04-11 22:20:01 +0200

    pallier 2 q2 ok

--- sodoku.py
+++ sodoku.py
@@ -1,57 +1,55 @@
-from tkinter import * #import de l'interface graphique
-from tkinter import messagebox
+from tkinter import *
 
 monSudoku = Tk()
 
-matriceUnsolved = [[0,0,0,2,0,0,4,0,0,],
-                  [0,0,7,0,0,3,0,1,2,],
-                  [0,2,1,4,0,0,0,6,3,],
-                  [7,0,0,0,0,0,8,5,1,],
-                  [8,0,4,0,0,0,3,0,6,],
-                  [1,3,2,0,0,0,0,0,4,],
-                  [5,1,0,0,0,4,2,9,0,],
-                  [6,4,0,9,0,0,1,0,0,],
-                  [0,0,9,0,0,5,0,0,1,]]
+with open("sudoku1.txt") as file_in: #Ouverture d'un fichier et affichage d'un sudoku partiellement résolu
+    sudokuUnsolved = []
+    lines = []
+    sudokus = file_in.read()
+    separate_arrays = sudokus.split(";")
+    array_without_break = separate_arrays[0].split("\n")
+    while '' in array_without_break:
+        array_without_break.remove('')
+    for line in array_without_break:
+        row = line.split(" ")
+        sudokuUnsolved.append(row)
+
 rows = [] #Mise en place de la matrice sur l'interface graphique
-for i in range(len(matriceUnsolved)):
+for i in range(len(sudokuUnsolved)):
     cols = []
-    for j in range(len(matriceUnsolved[i])):
+    for j in range(len(sudokuUnsolved[i])):
         e = Entry(relief=GROOVE, bg="lightblue")
         e.grid(row=i, column=j, sticky=NSEW)
-        e.insert(END, matriceUnsolved[i][j])
+        e.insert(END, sudokuUnsolved[i][j])
         cols.append(e)
     rows.append(cols)
 
-def VerifSolution() : #Fonction de vérification de la matrice
-    matriceSolved = [[3,6,5,2,7,1,4,8,9,],
-                    [4,8,7,6,9,3,5,1,2,],
-                    [9,2,1,4,5,8,7,6,3,],
-                    [7,9,6,3,4,2,8,5,1,],
-                    [8,5,4,7,1,9,3,2,6,],
-                    [1,3,2,5,8,6,9,7,4,],
-                    [5,1,3,8,6,4,2,9,7,],
-                    [6,4,8,9,2,7,1,3,5,],
-                    [2,7,9,1,3,5,6,4,8,]]
+def VerifSolution() : #Fonction de vérification de la matrice depuis l'ouverture d'un fichier
+    with open("sudoku1.txt") as file_in:
+        sudokuSolved = []
+        lines = []
+        sudokus = file_in.read()
+        separate_arrays = sudokus.split(";")
+        array_without_break = separate_arrays[1].split("\n")
+        while '' in array_without_break:
+            array_without_break.remove('')
+        for line in array_without_break:
+            row = line.split(" ")
+            sudokuSolved.append(row)
     check = 0
-    print("----------------")
-    for i in range(len(matriceSolved)):
-        for j in range(len(matriceSolved[i])):
-            if (str(matriceSolved[i][j]) != rows[i][j].get()):
-                print(rows[i][j].get())
+    for i in range(len(sudokuSolved)):
+        for j in range(len(sudokuSolved[i])):
+            if (str(sudokuSolved[i][j]) != rows[i][j].get()):
                 check += check + 1
                 rows[i][j].config(relief=GROOVE, bg="red")
             else:
-                print(rows[i][j].get())
                 rows[i][j].config(relief=GROOVE, bg="green")
-    print("----------------")
     if(check != 0):
        messagebox.showinfo("Résultat de la comparaison", "C'est perdu, essaye encore !")
     else:
-        messagebox.showinfo("Résultat de la comparaison", "C'est gagné, félicitations !")       
+        messagebox.showinfo("Résultat de la comparaison", "C'est gagné, félicitations !")    
 
 
 monBouton = Button(monSudoku, text ="Vérifier le sudoku", command = lambda: VerifSolution())
 monBouton.grid(row = 1, column=10 )
-
-
-monSudoku.mainloop()
+mainloop() 
--- test.py
+++ test.py
@@ -1,24 +1,61 @@
 from tkinter import *
 
-test = Tk()
+monSudoku = Tk()
 
-sudoku1 = open("sudoku1.txt", "r")
-str1 = sudoku1.read()
-x = str1.split(",")
-
+with open("sudoku1.txt") as file_in: #Ouverture d'un fichier et affichage d'un sudoku partiellement résolu
+    sudokuUnsolved = []
+    lines = []
+    sudokus = file_in.read()
+    separate_arrays = sudokus.split(";")
+    array_without_break = separate_arrays[0].split("\n")
+    while '' in array_without_break:
+        array_without_break.remove('')
+    for line in array_without_break:
+        row = line.split(" ")
+        sudokuUnsolved.append(row)
 
 rows = [] #Mise en place de la matrice sur l'interface graphique
-for i in range(len(x)):
-    j = 0
+for i in range(len(sudokuUnsolved)):
     cols = []
-    while j < 9:
+    for j in range(len(sudokuUnsolved[i])):
         e = Entry(relief=GROOVE, bg="lightblue")
         e.grid(row=i, column=j, sticky=NSEW)
-        e.insert(END, x[i][j])
+        e.insert(END, sudokuUnsolved[i][j])
         cols.append(e)
-rows.append(cols)
+    rows.append(cols)
 
-print(rows)
-        
+def VerifSolution() : #Fonction de vérification de la matrice depuis l'ouverture d'un fichier
+    with open("sudoku1.txt") as file_in:
+        sudokuSolved = []
+        lines = []
+        sudokus = file_in.read()
+        separate_arrays = sudokus.split(";")
+        array_without_break = separate_arrays[1].split("\n")
+        while '' in array_without_break:
+            array_without_break.remove('')
+        for line in array_without_break:
+            row = line.split(" ")
+            sudokuSolved.append(row)
+    check = 0
+    for i in range(len(sudokuSolved)):
+        for j in range(len(sudokuSolved[i])):
+            if (str(sudokuSolved[i][j]) != rows[i][j].get()):
+                check += check + 1
+                rows[i][j].config(relief=GROOVE, bg="red")
+            else:
+                rows[i][j].config(relief=GROOVE, bg="green")
+    if(check != 0):
+       messagebox.showinfo("Résultat de la comparaison", "C'est perdu, essaye encore !")
+    else:
+        messagebox.showinfo("Résultat de la comparaison", "C'est gagné, félicitations !")    
+
+o = 1
+def Save():
+    o+= 1
+monBouton = Button(monSudoku, text ="Vérifier le sudoku", command = lambda: VerifSolution())
+monBouton.grid(row = 1, column=10 )
+
+monBouton = Button(monSudoku, text ="Enregistrer la partie", command = lambda: Save())
+monBouton.grid(row = 1, column=11 )
 
 mainloop() 
